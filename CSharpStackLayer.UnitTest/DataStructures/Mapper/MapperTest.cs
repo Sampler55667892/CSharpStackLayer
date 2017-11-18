4d6131183eb804370f69d54e7f2cd2797938cdf4
Sampler55667892 <y50001028530003@yahoo.co.jp>
2017-11-18 14:54:13 +0900

    Mapper の追加

---
+++ CSharpStackLayer.UnitTest/DataStructures/Mapper/MapperTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpStackLayer.UnitTest.DataStructures
+{
+    [TestClass]
+    public class MapperTest
+    {
+        abstract class TestDomain
+        {
+            public string UserId;
+            public DateTime? ServiceStartDateTime { get; set; }
+            public DateTime? ServiceEndDateTime { get; set; }
+        }
+
+        abstract class TestResponse
+        {
+            public string userId { get; set; }
+            public string serviceStartDateTime { get; set; }
+            public string serviceEndDateTime { get; set; }
+        }
+
+        class AServiceDomain : TestDomain
+        {
+            public bool IsPremiumUser;
+            // 月額料金 (期間などによって変動)
+            public int CurrentMonthlyPrice { get; set; }
+        }
+
+        class AServiceResponse : TestResponse
+        {
+            public string isPremiumUser { get; set; }
+            public string currentMonthlyPrice { get; set; }
+        }
+
+        class BServiceDomain : TestDomain
+        {
+            // ユーザのサービス利用履歴に応じた商品メッセージ
+            public string CommercialMessage;
+        }
+
+        class BServiceResponse : TestResponse
+        {
+            public string commercialMessage { get; set; }
+        }
+
+        [TestMethod]
+        public void Map()
+        {
+            // マップの構成
+            var mapper = new Mapper<TestDomain, TestResponse>();
+            mapper.RecordAdditional<AServiceDomain, AServiceResponse>( (i, o) => {
+                o.serviceStartDateTime = ToDateString( i.ServiceStartDateTime );
+                o.serviceEndDateTime = ToDateString( i.ServiceEndDateTime );
+                o.isPremiumUser = i.IsPremiumUser ? "1" : "0";
+                o.currentMonthlyPrice = i.CurrentMonthlyPrice.ToString();
+            });
+            mapper.RecordAdditional<BServiceDomain, BServiceResponse>( (i, o) => {
+                o.serviceStartDateTime = ToDateString( i.ServiceStartDateTime );
+                o.serviceEndDateTime = ToDateString( i.ServiceEndDateTime );
+            });
+
+            // 入力を用意
+            var today = DateTime.Today;
+            var aDomain = new AServiceDomain {
+                UserId = "001",
+                ServiceStartDateTime = today,
+                ServiceEndDateTime = null,
+                IsPremiumUser = true,
+                CurrentMonthlyPrice = 1000
+            };
+            var bDomain = new BServiceDomain {
+                UserId = "001",
+                ServiceStartDateTime = today,
+                ServiceEndDateTime = today.AddDays( 10 ),
+                CommercialMessage = "ファミリーマートでプチりんごデニッシュ (4個入り) が発売"
+            };
+
+            // マップを適用して検証
+            var aResponse = mapper.Map<AServiceDomain, AServiceResponse>( aDomain );
+            aResponse.userId.Is( aDomain.UserId );
+            aResponse.serviceStartDateTime.Is( aDomain.ServiceStartDateTime?.ToString( "yyyy/MM/dd HH:mm:ss" ) );
+            aResponse.serviceEndDateTime.Is( aDomain.ServiceEndDateTime?.ToString( "yyyy/MM/dd HH:mm:ss" ) );
+            aResponse.isPremiumUser.Is( "1" );
+            aResponse.currentMonthlyPrice.Is( "1000" );
+
+            var bResponse = mapper.Map<BServiceDomain, BServiceResponse>( bDomain );
+            bResponse.userId.Is( bDomain.UserId );
+            bResponse.serviceStartDateTime.Is( bDomain.ServiceStartDateTime?.ToString( "yyyy/MM/dd HH:mm:ss" ) );
+            bResponse.serviceEndDateTime.Is( bDomain.ServiceEndDateTime?.ToString( "yyyy/MM/dd HH:mm:ss" ) );
+            bResponse.commercialMessage.Is( bDomain.CommercialMessage );
+        }
+
+        string ToDateString( DateTime? dt ) => dt?.ToString( "yyyy/MM/dd HH:mm:ss" );
+    }
+}
---
+++ CSharpStackLayer/DataStructures/Mapper/Mapper.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CSharpStackLayer
+{
+    // AutoMapper 風
+    // 　デフォルトマップとカスタムマップを分ける
+    // 　カスタムマップ優先でそれ以外はデフォルトマップを使う構成にする
+    public class Mapper<Domain, Codomain>
+    {
+        Dictionary<Type, Action<Domain, Codomain>> additionalMapRecords = new Dictionary<Type, Action<Domain, Codomain>>();
+
+        public void RecordAdditional<In, Out>( Action<In, Out> additionalMapAction )
+            where In: Domain
+            where Out: Codomain
+        {
+            // キーの重複があったらエラーにする
+            additionalMapRecords.Add( typeof( In ), (@in, @out) => additionalMapAction( (In)@in, (Out)@out ) );
+        }
+
+        public Out Map<In, Out>( In input )
+            where In: Domain
+            where Out: Codomain
+        {
+            // デフォルトマップ
+            var output = applyDefaultMap<In, Out>( input );
+
+            // (追加の) カスタムマップ
+            var additionalMapAction = GetMap<In>();
+            additionalMapAction( input, output );
+
+            return output;
+        }
+
+        // Reflection を使ってマップ可能な箇所はマップする
+        // 次の2つが一致する場合に値をコピーする
+        // 　(1) 名称 (小文字・大文字の区別なし)
+        //   (2) 型 (ほぼ完全一致)
+        Out applyDefaultMap<In, Out>( In input )
+            where In: Domain
+            where Out: Codomain
+        {
+            var output = Activator.CreateInstance<Out>();
+
+            // 見つからない場合は空配列が返る
+            var inProps = typeof( In ).GetProperties( BindingFlags.Instance | BindingFlags.Public );
+            var inFields = typeof( In ).GetFields( BindingFlags.Instance | BindingFlags.Public );
+
+            applyDefaultMapAtOutProperties<In, Out>( input, output, inProps, inFields );
+            applyDefaultMapAtOutFields<In, Out>( input, output, inProps, inFields );
+
+            return output;
+        }
+
+        // {In.Properties, In.Fields} -> Out.Properties
+        void applyDefaultMapAtOutProperties<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
+            where In: Domain
+            where Out: Codomain
+        {
+            var outProps = typeof( Out ).GetProperties( BindingFlags.Instance | BindingFlags.Public );
+
+            foreach (var op in outProps) {
+                var opLowerName = op.Name.ToLower();
+                var foundIp =
+                    inProps.FirstOrDefault(
+                        ip => string.Compare( ip.Name.ToLower(), opLowerName ) == 0 &&
+                        ip.PropertyType == op.PropertyType );
+                if (foundIp != null) {
+                    op.SetValue( output, foundIp.GetValue( input ) );
+                    // (小文字・大文字を除いて) 同じ名称で同じ型のプロパティとフィールドはクラススコープでは1つ
+                    continue;
+                }
+
+                var foundIf =
+                    inFields.FirstOrDefault(
+                        @if => string.Compare( @if.Name.ToLower(), opLowerName ) == 0 &&
+                        @if.FieldType == op.PropertyType );
+                if (foundIf != null)
+                    op.SetValue( output, foundIf.GetValue( input ) );
+            }
+        }
+
+        // {In.Properties, In.Fields} -> Out.Fields
+        void applyDefaultMapAtOutFields<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
+            where In: Domain
+            where Out: Codomain
+        {
+            var outFields = typeof( Out ).GetFields( BindingFlags.Instance | BindingFlags.Public );
+
+            foreach (var of in outFields) {
+                var ofLowerName = of.Name.ToLower();
+                var foundIp =
+                    inProps.FirstOrDefault(
+                        ip => string.Compare( ip.Name.ToLower(), ofLowerName ) == 0 &&
+                        ip.PropertyType == of.FieldType );
+                if (foundIp != null) {
+                    of.SetValue( output, foundIp.GetValue( input ) );
+                    continue;
+                }
+
+                var foundIf =
+                    inFields.FirstOrDefault(
+                        @if => string.Compare( @if.Name.ToLower(), ofLowerName ) == 0 &&
+                        @if.FieldType == of.FieldType );
+                if (foundIf != null)
+                    of.SetValue( output, foundIf.GetValue( input ) );
+            }
+        }
+
+        Action<Domain, Codomain> GetMap<In>()
+            where In: Domain
+        {
+            return additionalMapRecords[ typeof( In ) ];
+        }
+    }
+}

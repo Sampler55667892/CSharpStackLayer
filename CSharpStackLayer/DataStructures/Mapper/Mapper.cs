68fc492a0cd9da36fa54836ef50b90784b9657e9
Sampler55667892 <y50001028530003@yahoo.co.jp>
2017-11-18 15:59:28 +0900

    Mapper のリファクタ

--- CSharpStackLayer/DataStructures/Mapper/Mapper.cs
+++ CSharpStackLayer/DataStructures/Mapper/Mapper.cs
@@ -25,11 +25,10 @@ namespace CSharpStackLayer
             where Out: Codomain
         {
             // デフォルトマップ
-            var output = applyDefaultMap<In, Out>( input );
+            var output = ApplyDefaultMap<In, Out>( input );
 
             // (追加の) カスタムマップ
-            var additionalMapAction = GetMap<In>();
-            additionalMapAction( input, output );
+            GetAdditionalMap<In>()?.Invoke( input, output );
 
             return output;
         }
@@ -38,7 +37,7 @@ namespace CSharpStackLayer
         // 次の2つが一致する場合に値をコピーする
         // 　(1) 名称 (小文字・大文字の区別なし)
         // 　(2) 型 (完全一致)
-        Out applyDefaultMap<In, Out>( In input )
+        Out ApplyDefaultMap<In, Out>( In input )
             where In: Domain
             where Out: Codomain
         {
@@ -48,14 +47,14 @@ namespace CSharpStackLayer
             var inProps = typeof( In ).GetProperties( BindingFlags.Instance | BindingFlags.Public );
             var inFields = typeof( In ).GetFields( BindingFlags.Instance | BindingFlags.Public );
 
-            applyDefaultMapAtOutProperties<In, Out>( input, output, inProps, inFields );
-            applyDefaultMapAtOutFields<In, Out>( input, output, inProps, inFields );
+            ApplyDefaultMapAtOutProperties<In, Out>( input, output, inProps, inFields );
+            ApplyDefaultMapAtOutFields<In, Out>( input, output, inProps, inFields );
 
             return output;
         }
 
         // {In.Properties, In.Fields} -> Out.Properties
-        void applyDefaultMapAtOutProperties<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
+        void ApplyDefaultMapAtOutProperties<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
             where In: Domain
             where Out: Codomain
         {
@@ -64,8 +63,8 @@ namespace CSharpStackLayer
             foreach (var op in outProps) {
                 var opLowerName = op.Name.ToLower();
                 var foundIp =
-                    inProps.FirstOrDefault(
-                        ip => string.Compare( ip.Name.ToLower(), opLowerName ) == 0 &&
+                    inProps.FirstOrDefault( ip =>
+                        string.Compare( ip.Name.ToLower(), opLowerName ) == 0 &&
                         ip.PropertyType == op.PropertyType );
                 if (foundIp != null) {
                     op.SetValue( output, foundIp.GetValue( input ) );
@@ -74,8 +73,8 @@ namespace CSharpStackLayer
                 }
 
                 var foundIf =
-                    inFields.FirstOrDefault(
-                        @if => string.Compare( @if.Name.ToLower(), opLowerName ) == 0 &&
+                    inFields.FirstOrDefault( @if =>
+                        string.Compare( @if.Name.ToLower(), opLowerName ) == 0 &&
                         @if.FieldType == op.PropertyType );
                 if (foundIf != null)
                     op.SetValue( output, foundIf.GetValue( input ) );
@@ -83,7 +82,7 @@ namespace CSharpStackLayer
         }
 
         // {In.Properties, In.Fields} -> Out.Fields
-        void applyDefaultMapAtOutFields<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
+        void ApplyDefaultMapAtOutFields<In, Out>( In input, Out output, PropertyInfo[] inProps, FieldInfo[] inFields )
             where In: Domain
             where Out: Codomain
         {
@@ -92,8 +91,8 @@ namespace CSharpStackLayer
             foreach (var of in outFields) {
                 var ofLowerName = of.Name.ToLower();
                 var foundIp =
-                    inProps.FirstOrDefault(
-                        ip => string.Compare( ip.Name.ToLower(), ofLowerName ) == 0 &&
+                    inProps.FirstOrDefault( ip =>
+                        string.Compare( ip.Name.ToLower(), ofLowerName ) == 0 &&
                         ip.PropertyType == of.FieldType );
                 if (foundIp != null) {
                     of.SetValue( output, foundIp.GetValue( input ) );
@@ -101,15 +100,15 @@ namespace CSharpStackLayer
                 }
 
                 var foundIf =
-                    inFields.FirstOrDefault(
-                        @if => string.Compare( @if.Name.ToLower(), ofLowerName ) == 0 &&
+                    inFields.FirstOrDefault( @if =>
+                        string.Compare( @if.Name.ToLower(), ofLowerName ) == 0 &&
                         @if.FieldType == of.FieldType );
                 if (foundIf != null)
                     of.SetValue( output, foundIf.GetValue( input ) );
             }
         }
 
-        Action<Domain, Codomain> GetMap<In>()
+        Action<Domain, Codomain> GetAdditionalMap<In>()
             where In: Domain
         {
             return additionalMapRecords[ typeof( In ) ];
